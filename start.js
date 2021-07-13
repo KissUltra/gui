c12e3129f1189f8f192e0450632ed5e4f6fb795c
Alexander Fedorov <fedor@anuta.org>
2021-07-13 09:08:33 +0200

    change min size for nw2

--- start.js
+++ start.js
@@ -4,8 +4,8 @@ nw.Window.open('./main.html', {
     'id': 'kissultragui',
     'title': 'KISS ULTRA GUI ' + pkg.version,
     'height': 600,
-    'width': 970,
-    "min_width": 900,
+    'width': 1000,
+    "min_width": 1000,
     "min_height": 600,
     'position': 'center',
     'resizable': true
