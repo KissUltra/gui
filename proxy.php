163d50fe8c5281111403f131074e7e62616e9bcf
Alexander Fedorov <fedor@anuta.org>
2021-08-17 15:32:38 +0200

    More hardening. Will not harm.

--- proxy.php
+++ proxy.php
@@ -6,7 +6,11 @@
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS | CURLPROTO_HTTP);
+        curl_setopt($ch, CURLOPT_REDIR_PROTOCOLS, CURLPROTO_HTTPS);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         curl_exec($ch);
         curl_close($ch);
 	}
