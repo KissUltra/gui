a674fb7a4787ba37ddd94ac557f0ae0bc015a588
Alexander Fedorov <fedor@anuta.org>
2021-08-17 15:28:11 +0200

    Allow only HTTP protocols

--- proxy.php
+++ proxy.php
@@ -6,6 +6,7 @@
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS | CURLPROTO_HTTP);
         curl_exec($ch);
         curl_close($ch);
 	}
